c691da36d0d92269b8159eab147ed7bac3cdba9f
root <root@DESKTOP-ELINHUL.localdomain>
2018-02-26 09:36:45 +0100

    post route

--- controllers/chatStart.js
+++ controllers/chatStart.js
@@ -1,8 +1,9 @@
 var Bot_basic = require ('../models/bot_basic');
 
 var async = require('async');
+var bodyParser = require("body-parser");
 
-exports.index = function(req, res, next) {  
-        console.log("are we here?")
-        res.render('chat', { title: 'Bots', bots_list: list_bots});
+exports.index = function(req, res, next) { 
+        console.log("are we here?");
+        res.send("this will be the chat ");
 };
--- routes/bot.js
+++ routes/bot.js
@@ -19,15 +19,14 @@ var chat_start = require('../controllers/chatStart');
 router.get('/', bot_controller.index)
 
 // GET chat app
-router.get('/chat', function (req, res) {
-    res.send("this will be the chat ");
-  })
+router.get('/chat', chat_start.index) 
 
-  router.post('/chat', function (req, res) {
-    var bot1=req.body.bot1;
-    var bot2 = req.body.bot2
-    console.log("Der name der ausgewählten bots ist " + bot1 + " und " + bot2);
-  })
+
+router.post('/chat', function (req, res) {
+  var bot1=req.body.bot1;
+  var bot2 = req.body.bot2
+  console.log("Der name der ausgewählten bots ist " + bot1 + " und " + bot2);
+})
 
   
 
