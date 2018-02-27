0284a6c81df806d79e2445178086151cffd7720f
aljosazidan <root@DESKTOP-ELINHUL.localdomain>
2018-02-27 12:00:58 +0100

    just some comments

--- public/javascripts/select_click.js
+++ public/javascripts/select_click.js
@@ -5,32 +5,51 @@ var bot_array =[0,0];
 $( document ).ready(function() {
   console.log( "ready!" );
 
+// if you click on bots
   $( ".klick" ).on("click", function() {
-    counter = counter + 1;
-    if (counter == 1) {      
+
+    //count clicks
+    counter += 1;
+    
+    if (counter == 1) {   
+      
+      //change color on clicked bot
       $(this).toggleClass('clicked');
+
+      //save bot name to the array
       bot_array[0] = $(this).children('h5').text();
-      console.log("click 1:" + counter)
       console.log(bot_array);
     }
+
+    //counter 
     if (counter == 2) {      
+
+      //change color on clicked bot
       $(this).toggleClass('clicked');
+
+      //save bot name to the bot_array
       bot_array[1] = $(this).children('h5').text();
-      console.log("click 2;" + counter)
       console.log(bot_array);
+
+      ////change "suche bots to chat now"
       $( ".chat-start h1" ).replaceWith( "<h1>Chat Now!</h1>" );
 
     }
+    //if more than two clicks happen
     if (counter > 2 ) {
+      //change color on selected bots back to start color
       $('.klick').removeClass('clicked');
+
+      //change "chat now" back to >Bitte Wähle zwei Bots aus:
       $( ".chat-start h1" ).replaceWith( "<h1>Bitte Wähle zwei Bots aus:</h1>" );
+
+      //reset bot_array & counter
       bot_array = [0,0];
       counter = 0;
-      console.log(counter);
       console.log(bot_array);
     }
   }); 
-
+  //if two clicks happpen, than you can click on the Chat button
   $(".chat-start" ).on("click", function() {
     if(counter == 2) {
     console.log("Send Bot information to the server!");
