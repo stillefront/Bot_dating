000df0e60945a97560d6832c14b8e1aa0533b385
aljosazidan <aljosa.zidan@campus.tu-berlin.de>
2018-04-02 18:00:14 +0200

    little corrections

--- controllers/socketServer.js
+++ controllers/socketServer.js
@@ -1,5 +1,5 @@
 exports = module.exports = function(io){
     io.on('connection',function(socket){
-        console.log('socket.io is running!!!!');
+        console.log('socket.io is running!!!!');    
     }
     )};
