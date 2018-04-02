9e29231c685aa1b2b14719a9da4c4fbf6c0db95a
aljosazidan <aljosa.zidan@campus.tu-berlin.de>
2018-04-02 20:28:06 +0200

    socket.io connected

--- controllers/socketServer.js
+++ controllers/socketServer.js
@@ -1,5 +1,16 @@
+//this is the server side of socket.io
+
 exports = module.exports = function(io){
     io.on('connection',function(socket){
-        console.log('socket.io is running!!!!');    
+        console.log('a user connected');  
+        
+        
+        socket.on('disconnect', function(){
+            console.log('user disconnected');
+          });
+
+        socket.on('chat message', function(msg){
+            console.log('message: ' + msg);
+        });
     }
     )};
--- public/javascripts/socketClient.js
+++ public/javascripts/socketClient.js
@@ -1,2 +1,8 @@
-var socket = io();
-console.log("read the socketClient.js")
+//this is the client side of socket.io 
+
+
+$( document ).ready(function() {
+
+    var socket = io();
+    console.log("read the socketClient.js");
+});
